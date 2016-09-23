6dedc8ad1b6e05918489f9cf9fa04124a65ddae0
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-23 21:16:16 +0100

    Add EnemyController

---
+++ Assets/scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed;
+
+	private void Update () {
+		Move ();
+	}
+
+	private void Move () {
+		transform.Translate (Vector3.left * moveSpeed);
+	}
+}
