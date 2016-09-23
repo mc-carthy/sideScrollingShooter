9a39f83252b8e8317280f785c3b67d26d1317e10
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-23 20:35:52 +0100

    Add clamped player movement

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -3,5 +3,26 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	
+	[SerializeField]
+	private float moveSpeed;
+	private float xClamp = 16, yClamp = 8.5f;
+
+	private void Update () {
+		Move ();
+		ClampMovement ();
+	}
+
+	private void Move () {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		Vector3 movementVector = new Vector3 (h, v, 0).normalized;
+		transform.Translate (movementVector * moveSpeed, Space.World);
+	}
+
+	private void ClampMovement () {
+		Vector3 temp = transform.position;
+		temp.x = Mathf.Clamp (temp.x, -xClamp, xClamp);
+		temp.y = Mathf.Clamp (temp.y, -yClamp, yClamp);
+		transform.position = temp;
+	}
 }
