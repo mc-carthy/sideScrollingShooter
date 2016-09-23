baaaa2d000de8a6728428c56eafb8471bf73c2cd
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-23 20:47:00 +0100

    Add projectile firing and destruction

---
+++ Assets/ProjectileController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed = 5;
+
+	private void Start () {
+		Destroy (gameObject, 5);
+	}
+
+	private void Update () {
+		transform.Translate (Vector3.right * moveSpeed);
+	}
+}
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	[SerializeField]
+	private GameObject projectile;
+	[SerializeField]
 	private float moveSpeed;
 	private float xClamp = 16, yClamp = 8.5f;
 
 	private void Update () {
 		Move ();
 		ClampMovement ();
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			SpawnProjectile();
+		}
 	}
 
 	private void Move () {
@@ -24,5 +29,13 @@ public class PlayerController : MonoBehaviour {
 		temp.x = Mathf.Clamp (temp.x, -xClamp, xClamp);
 		temp.y = Mathf.Clamp (temp.y, -yClamp, yClamp);
 		transform.position = temp;
+	}
+
+	private void SpawnProjectile () {
+		Instantiate (
+			projectile,
+			transform.position,
+			Quaternion.identity
+		);
 	}
 }
